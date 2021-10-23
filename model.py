eeeac8cb5eee5e09b5bcf20bcc5b41a4a1795b55
Tasha Davis <tdavis0831@gmail.com>
2021-10-23 15:08:33 +0000

    json file created, crud.py includes creating a user, working on seed_database.py

--- model.py
+++ model.py
@@ -11,10 +11,33 @@ class PayerAccountInfo(db.model):
 
     payer = db.Column(db.String)
     points = db.Column(db.Integer)
-    date = db.Column(db.DateTime)
+    timestamp = db.Column(db.DateTime)
 
     #repr function to show us information that is valueable
     def __repr__(self):
-        return f"Payer payer_string={self.payer} points={self.points} date ={self.date}"
+        return f"Payer payer_string={self.payer} points={self.points} date ={self.timestamp}"
 
-        
+
+
+
+#this function connects to the database named fetch that was created the rest of this code is used to 
+#Initalize the database
+def connect_to_db(flask_app, db_uri="postgresql:///fetch", echo=False):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    #added this to vizualize connection was sucessful
+    print("connected")
+
+
+if __name__ == "__main__":
+    from server import app
+
+
+
+    connect_to_db(app)
+
